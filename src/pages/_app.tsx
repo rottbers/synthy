6c928ab31849ea1da4b5cad99000dc74754e388c
Johan Rottbers <johan@rottbers.com>
2021-05-06 16:51:16 +0200

    Adds ogimage (#9)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,9 @@ import '../tailwind.css';
 const App = ({ Component, pageProps }: AppProps) => (
   <>
     <Head>
+      <meta property="og:image" content="https://synthy.netlify.app/ogimage.png" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
       <link rel="icon" href="/favicon.ico" />
       <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
       <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
